65de6599288288ced21add9890463085e0112ca0
DehSartoreto <fixsartoreto@gmail.com>
2020-07-13 19:32:13 -0300

    organizando o GitHub da forma correta, removendo pastas

--- exercises/shell-script/7.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [ -d $1 ]
-then
-    FILE=`ls -l $1 | wc -l`
-    echo "O $1 tem  $FILE arquivos."
-else
-    echo "O argumento $1 não é um diretorio."
-fi 
--- exercises/shell-script/b3.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-DATA=$(date +%F)
-for FILE in `ls *.png`
-do
-    mv $FILE ${DATA}-${FILE}
-done
