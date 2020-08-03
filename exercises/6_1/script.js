ceee0cc3f818a573c5d8525d1b1445b1ca79d77d
DehSartoreto <fixsartoreto@gmail.com>
2020-08-03 18:06:01 -0300

    finalizando o exercicio

--- exercises/6_1/script.js
+++ exercises/6_1/script.js
@@ -1,6 +1,7 @@
 window.onload = function() {
     let estados = ['AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MG', 'MS', 'MT', 'PA', 'PB', 'PE', 'PI', 'PR', 'RJ', 'RN', 'RO', 'RR', 'RS', 'SC', 'SE', 'SP', 'TO'];
     let stateElement = document.querySelector(".state");
+    let selectForm = document.querySelector(".myform");
 
     for (let i = 0; i < estados.length; i += 1) {
     let estado = document.createElement('option');
@@ -11,6 +12,27 @@ window.onload = function() {
 
     let elemButton = document.getElementById('enviar');
     elemButton.addEventListener('click', function (event) {
-    event.preventDefault();
+    event.preventDefault();   
+    createDiv(); 
     });
+
+    let resetButton = document.querySelector("#limpar");
+    resetButton.addEventListener("click", function() {
+        selectForm.reset();
+    })
+
+    function createDiv() {
+        let allInputs = document.querySelectorAll("input");
+        let content = "";
+        for (let i = 0; i < allInputs.length; i += 1){
+            content += allInputs[i].name + ": " + allInputs[i].value + "\n";
+        }
+        let divField = document.createElement("fieldset");
+        let divConsolidado = document.createElement("div");
+        divConsolidado.innerText = content;
+        selectForm.appendChild(divField);
+        divField.appendChild(divConsolidado);
+
+    }
+    
 }
