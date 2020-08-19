5d4d37f9467719e0fb42328a9da68028572852b9
DehSartoreto <fixsartoreto@gmail.com>
2020-08-19 16:09:31 -0300

    exercises reduce

---
+++ exercises/8_3/exercise1.js
@@ -0,0 +1,16 @@
+    
+const assert = require('assert');
+
+const arrays = [
+    ["1", "2", "3"],
+    [true],
+    [4, 5, 6]
+];
+
+
+function flatten() {
+  // escreva seu código aqui
+  return arrays.reduce((result, array) => result.concat(array), [])
+}
+
+assert.deepEqual(flatten(), ["1", "2", "3", true, 4, 5, 6]);
