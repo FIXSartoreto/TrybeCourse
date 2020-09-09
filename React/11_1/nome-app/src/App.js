7aa63cd5dde666451e8436e12fbd94da90bf5b85
DehSartoreto <fixsartoreto@gmail.com>
2020-09-09 18:37:07 -0300

    exercicios React

--- React/11_1/nome-app/src/App.js
+++ React/11_1/nome-app/src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const taskList = ['Trabalhar', 'Estudar', "Namorar", 'Beber', 'Me Divertir', 'Dormir'];
+
+const task = (value) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <li>{value}</li>,
+    taskList.map(task => <li>{task}</li>)    
   );
+}
+
+
+
+function App() {
+  return task('oaushdousaodsahuohadohdaohdouhaduohduduoshou');
 }
 
 export default App;
