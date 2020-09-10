5f12e7755c016ef861a04b7f16375b448df81870
DehSartoreto <fixsartoreto@gmail.com>
2020-09-10 18:52:10 -0300

    pokedex

---
+++ React/11_2/my-pokedex/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './App.css';
+import Pokedex from './pokedex';
+
+function App() {
+  return (
+    <div className='App'>
+      <Pokedex />
+    </div>
+  );
+}
+
+export default App;
