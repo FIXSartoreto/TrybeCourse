14cce7958b1f043c9dccc5ea888ec21b76651db0
DehSartoreto <fixsartoreto@gmail.com>
2020-09-21 19:19:35 -0300

    exercise

---
+++ React/13_1/13_1_1/exercise-dog-image/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import RequestAPI from './RequestAPI'
+
+function App() {
+  return (
+    <div className="App">
+      <RequestAPI />
+    </div>
+  );
+}
+
+export default App;
